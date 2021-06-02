cd3028b00ca2fe03ab0af510de19335b7629049a
Shaswatadatta7 <shaswatadatta2000@gmail.com>
2021-06-02 22:35:55 +0530

    Fast Reading feature

--- code.java
+++ code.java
@@ -1,9 +1,61 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 class git{
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
+		//Scanner sc = new Scanner (System.in);
+		FastReader sc = new FastReader();
 		int a =sc.nextInt();
 
 		System.out.println(a);
 	}
+
+
 }
+class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
